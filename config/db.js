38d13398fc09d3f5f0b2cf769b659e92d270197c
Ben Chavez <ben.m.chavez@gmail.com>
2023-02-08 15:02:53 -0600

    added user registration logic

--- config/db.js
+++ config/db.js
@@ -9,6 +9,7 @@ const connectDB = async () => {
 
     await mongoose.connect(db, {
       useNewUrlParser: true,
+      // useCreateIndex: true,
     });
 
     console.log("MongoDB Connected...");
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require("express-validator/check");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
+
+// Bring in the User Model:
+const User = require("../../models/User");
 
 // @route   GET api/users
 // @desc    Test route
@@ -20,7 +25,7 @@ router.post(
       "Please enter a password with 6 or more characters"
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     // Need to init middleware in the server.js file to get data in req.body
     // console.log(req.body);
 
@@ -30,7 +35,51 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send("User route");
+    // Destructure the request body
+    const { name, email, password } = req.body;
+
+    try {
+      // See if user exists
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      // Get user's gravatar (based on email)
+      const avatar = gravatar.url(email, {
+        s: "200", // default size
+        r: "pg", // rating
+        d: "mm", // default image
+      });
+
+      // Create new user instance (not saved yet)
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      // Generate Bycrypt Hashing Salt for password hashing
+      const salt = await bcrypt.genSalt(10);
+
+      // Encrypt the password using bycrypt
+      user.password = await bcrypt.hash(password, salt);
+
+      // Save user to database
+      await user.save();
+
+      // Return the jsonwebtoken (to auto login users after acct registration)
+      res.send("User registered");
+
+      res.send("User route");
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server error");
+    }
   }
 );
 
