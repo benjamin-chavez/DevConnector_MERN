a4370af817ee45b506834552a0e5671437cccd77
Ben Chavez <ben.m.chavez@gmail.com>
2023-02-12 21:23:28 -0600

    Added Profile Route to DELETE an experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -233,5 +233,28 @@ router.put(
   }
 );
 
+// @route   DELETE api/profile/experience (could technically be a PUT instead)
+// @desc    Delete Experience from Profile
+// @access  Private
+router.delete('/experience/:experience_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    // Get the remove index
+    const removeIndex = profile.experience
+      .map((item) => item.id)
+      .indexOf(req.params.exp_id);
+
+    profile.experience.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 // Export the router
 module.exports = router;
