f38e98f7687ceff41e27882ca2ace440872fc6a6
Ben Chavez <ben.m.chavez@gmail.com>
2023-02-18 15:59:11 -0600

    Added PrivateRoute to protect selected routes. Applied the PrivateRoute to our user Dashboard component

---
+++ frontend/src/components/dashboard/Dashboard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// import { connect } from 'react-redux';
+
+import PropTypes from 'prop-types';
+
+const Dashboard = (props) => {
+  return (
+    <section className="container">
+      <div>Dashboard</div>
+    </section>
+  );
+};
+
+Dashboard.propTypes = {};
+
+export default Dashboard;
---
+++ frontend/src/components/routing/PrivateRoute.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const PrivateRoute = ({
+  component: Component,
+  auth: { isAuthenticated, loading },
+  ...rest
+}) => {
+  if (!isAuthenticated && !loading) return <Navigate to="/login" />;
+
+  return <Component />;
+};
+
+PrivateRoute.propTypes = {
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps)(PrivateRoute);
