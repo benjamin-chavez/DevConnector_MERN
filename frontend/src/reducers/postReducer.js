c07c7d2ae1624e7cb52a0c99c1bb5022719b8de5
Ben Chavez <ben.m.chavez@gmail.com>
2023-02-21 23:47:07 -0600

    Added DELETE_POST functionality

--- frontend/src/reducers/postReducer.js
+++ frontend/src/reducers/postReducer.js
@@ -1,4 +1,9 @@
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from '../actions/types';
+import {
+  GET_POSTS,
+  POST_ERROR,
+  UPDATE_LIKES,
+  DELETE_POST,
+} from '../actions/types';
 
 const initialState = {
   posts: [],
@@ -17,10 +22,10 @@ function postReducer(state = initialState, action) {
         posts: payload,
         loading: false,
       };
-    case POST_ERROR:
+    case DELETE_POST:
       return {
         ...state,
-        error: payload,
+        posts: state.posts.filter((post) => post._id !== payload),
         loading: false,
       };
     case UPDATE_LIKES:
@@ -32,6 +37,13 @@ function postReducer(state = initialState, action) {
         ),
         loading: false,
       };
+    case POST_ERROR:
+      return {
+        ...state,
+        error: payload,
+        loading: false,
+      };
+
     default:
       return state;
   }
